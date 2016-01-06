6bdf1656bce071004e1f46136262fb8dc2caddf6
headlessme <andy@coggle.it>
2016-01-06 02:15:36 +0000

    add bower file

---
+++ bower.js
@@ -0,0 +1,15 @@
+{
+  "name": "backbone.captureLinks",
+  "main": "./backbone.captureLinks.js",
+  "dependencies": {
+    "backbone": "~1.x.x"
+  },
+  "ignore": [
+    "examples",
+    "spec",
+    "package.json",
+    ".travis.yml",
+    "CNAME",
+    "Makefile"
+  ]
+}
