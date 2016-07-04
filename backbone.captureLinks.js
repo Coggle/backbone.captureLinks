c237a493d58aaba2976b8a85c342624f11450f1f
headlessme <andy@coggle.it>
2016-07-04 15:15:48 +0100

    allow disabling

--- backbone.captureLinks.js
+++ backbone.captureLinks.js
@@ -6,6 +6,13 @@ Backbone.Router.captureLinks = function() {
   // method, to be processed by the router. If the link has a `data-bypass`
   // attribute, bypass the delegation completely.
   $(document).on("click", "a[href]:not([data-bypass])", function(evt) {
+    if (Backbone.Router._disableCaptureLinks) return;
+    // if this looks like a click with a modifier to open-in-new-tab, then
+    // don't route it in this tab:
+    if (evt.metaKey || evt.ctrlKey || evt.shiftKey) {
+        return;
+    }
+
     // Get the absolute anchor href.
     var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
     
@@ -20,9 +27,13 @@ Backbone.Router.captureLinks = function() {
       // `Backbone.history.navigate` is sufficient for all Routers and will
       // trigger the correct events. The Router's internal `navigate` method
       // calls this anyways.  The fragment is sliced from the root.
-      Backbone.history.navigate(href.attr, true);
+      Backbone.history.navigate(href.prop.slice(root.length, href.prop.length), true);
     }
   });
+};
+
+Backbone.Router.disableCaptureLinks = function() {
+  Backbone.Router._disableCaptureLinks = true;
 };
 
 });
